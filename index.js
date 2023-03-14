ef12fd3ea63af0903584d42c518c87182f0fe5fb
Kiruidyno <kirui472@gmail.com>
2023-03-14 20:34:03 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ let num2 = 31;
 let multiply = num1 * num2;
 console.log(multiply);
 
-
 let random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+console.log(random)
 
 
-let num3 = 20;
-let num4 = 8;
-let mod = num3 % num4;
-
+let num3 = 8;
+let num4 = 20;
+let mod = num4 % num3;
+console.log(mod)
 
 
 let num5 = 6;
@@ -19,5 +19,5 @@ let num6 = 8;
 let num7 = 11;
 let num8 = 20;
 let max = Math.max(num5, num6, num7, num8);
-max; 
+console.log(max); 
 
